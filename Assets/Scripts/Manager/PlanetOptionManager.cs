59fdda754d4f68e536fd58a01fb9117a074e51ba
LSH <lharu138@naver.com>
2025-11-18 20:07:02 +0900

    [fix] 행성 생성 및 초기화 작업 수행하도록 수정

--- Assets/Scripts/Manager/PlanetOptionManager.cs
+++ Assets/Scripts/Manager/PlanetOptionManager.cs
@@ -1,6 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
+using Planet.Data;
+using Planet.Base;
+using System.Linq;
 
 namespace Planet.Manager
 {
@@ -28,12 +30,26 @@ namespace Planet.Manager
         public static float SpeedMultify => Instance._speedMultifyValue;
         public static float DistanceMultify => Instance._distanceMultifyValue;
 
+        public static string[] PlanetList => Instance._planetList;
+
+        [Header("青己 单捞磐")]
+        [Tooltip("青己 单捞磐 格废")]
+        [SerializeField] PlanetInfo _planetInfo;
+
+        [Tooltip("怕剧 Trasnform")]
+        [SerializeField] Transform _sunTransform;
+
+        [Tooltip("青己 坷宏璃飘 何葛 Trasnform")]
+        [SerializeField] Transform _planetParent;
+
         [Header("青己 可记")]
         [Tooltip("青己 傍傈 加档 硅啦")]
         [SerializeField] float _speedMultifyValue = 1;
 
         [Tooltip("青己 芭府 硅啦")]
         [SerializeField] float _distanceMultifyValue = 1;
+
+        private string[] _planetList;
 
         public void Awake()
         {
@@ -44,6 +60,27 @@ namespace Planet.Manager
             }
 
             DontDestroyOnLoad(gameObject);
+
+            //鞘荐 亲格 眉农
+            Assert.IsNotNull(_sunTransform, "SunTransform is Null");
+            Assert.IsNotNull(_planetInfo, "PlanetInfo is Null");
+
+            //青己 积己
+            CreatePlanets();
+
+            //青己 府胶飘 累己
+            _planetList = _planetInfo.planetDatas.Select(p => p.Name).ToArray();
+        }
+
+        private void CreatePlanets()
+        {
+            PlanetData[] datas = _planetInfo.planetDatas;
+            foreach (var data in datas)
+            {
+                GameObject planetObj = Instantiate(data.Model, _planetParent);
+                PlanetBase planetBase = planetObj.AddComponent<PlanetBase>();
+                planetBase.Initialize(data, _sunTransform);
+            }
         }
     }
 }

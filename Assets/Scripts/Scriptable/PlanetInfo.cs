ab452647ce3a2d350a8f3f80693bd9994bf876c3
LSH <lharu138@naver.com>
2025-11-18 19:46:26 +0900

    [feat] 행성 정보 입력을 위한 ScriptableObject 작성

---
+++ Assets/Scripts/Scriptable/PlanetInfo.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Planet.Data
+{
+    [Serializable]
+    public class PlanetData
+    {
+        [Header("기본 설정")]
+        [FormerlySerializedAs("모델")]
+        public GameObject Model;
+
+        [Tooltip("이름")]
+        public string Name;
+
+        [Tooltip("근일점(AU)")]
+        public float Perihelion;
+
+        [Tooltip("원일점(AU)")]
+        public float Aphelion;
+
+        [Tooltip("근일점 경도")]
+        public float PerihelionAngle;
+
+        [Tooltip("공전 주기(년)")]
+        public float SiderealPeriod;
+    }
+
+    [CreateAssetMenu(fileName = "PlanetInfo.asset", menuName = "Planet/PlanetInfo")]
+    public class PlanetInfo : ScriptableObject
+    {
+        public PlanetData[] planetDatas;
+    }
+
+}
